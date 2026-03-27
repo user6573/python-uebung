730f6c48e193360287e827970493e569614f2f91
Fritz-derEchte <173768095+Fritz-derEchte@users.noreply.github.com>
2026-03-27 10:53:18 +0100

    Add modulo function

--- calculator.py
+++ calculator.py
@@ -12,8 +12,12 @@ def divide(a, b):
         raise ValueError("Division durch Null nicht erlaubt")
     return a / b
 
+def modulo(a, b):
+    return a % b
+
 if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", subtract(5, 3))
     print("5 * 3 =", multiply(5, 3))
     print("6 / 3 =", divide(6, 3))
+    print("7 % 3 =", modulo(7, 3))
