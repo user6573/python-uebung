79e61ad80ee85e287002581e2144abe7177e3414
Fritz-derEchte <173768095+Fritz-derEchte@users.noreply.github.com>
2026-03-27 10:53:35 +0100

    Add power function

--- calculator.py
+++ calculator.py
@@ -15,9 +15,13 @@ def divide(a, b):
 def modulo(a, b):
     return a % b
 
+def power(a, b):
+    return a ** b
+
 if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", subtract(5, 3))
     print("5 * 3 =", multiply(5, 3))
     print("6 / 3 =", divide(6, 3))
     print("7 % 3 =", modulo(7, 3))
+    print("2 ^ 8 =", power(2, 8))
